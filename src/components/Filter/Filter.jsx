701ebc46a4dcf37974b913e456f7db4bb9c889b9
SuperNiceCat <131773661+SuperN1ceC6t@users.noreply.github.com>
2024-01-19 17:34:56 +0200

    First Commit

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,21 @@
-export const Filter = ({value ,filterContacts}) => {
-    return (
-        <>
-            <p>Find contacts by name</p>
-            <input type="text" name="filter" onChange={filterContacts} required/>
-        </>
-    )
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter, getFilterValue } from 'redux/filterSlice';
+import { StyledLabel, StyledInput } from './Filter.styled';
+
+function Filter() {
+  const dispatch = useDispatch();
+  const filterValue = useSelector(getFilterValue);
+  return (
+    <StyledLabel>
+      Find contacts by name
+      <StyledInput
+        name="filter"
+        type="text"       
+        onChange={event => dispatch(setFilter(event.currentTarget.value))}
+        value={filterValue}
+      />
+    </StyledLabel>
+  );
 }
+
+export default Filter;
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,5 +1,12 @@
-export const Message = ({message}) => {
-    return (
-        <p>{ message}</p>
-    )
+import PropTypes from 'prop-types';
+import { MessageText } from './Message.styled';
+
+function Message({ message }) {
+  return <>{message && <MessageText>{message}</MessageText>}</>;
 }
+
+Message.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default Message;
